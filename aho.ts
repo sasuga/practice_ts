85736bccc2b5d01231a01f0c1c1f8a24f06bd279
hidenobu nagao <hidenobu.nagao@gmail.com>
2021-09-11 13:57:38 +0900

    世界のナベアツ

---
+++ aho.ts
@@ -0,0 +1,11 @@
+function aho():void {
+	//3のつく数字と3の倍数はアホになります。
+	for(var i=1;i<100;i++) {
+		if(i%3==0 || String(i).match(/3/)) {
+			console.log("あほ");
+		} else {
+			console.log(String(i));
+		}
+	}
+}
+aho();
